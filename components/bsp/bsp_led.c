f88bcdf62c2cd845404757ef12c6468ad3fcceda
dkrasutski <krasutski.denis@gmail.com>
2025-10-07 19:23:51 +0000

    fix(blue-led): fix blue led get status

--- components/bsp/bsp_led.c
+++ components/bsp/bsp_led.c
@@ -6,11 +6,16 @@
 
 /* -------------------------------------------------------------------------- */
 
+static bool _is_blue_led_on = false;
+
+/* -------------------------------------------------------------------------- */
+
 void bsp_led_init(void) {
 
     gpio_reset_pin(BSP_PIN_BLUE_LED);
     gpio_set_direction(BSP_PIN_BLUE_LED, GPIO_MODE_OUTPUT);
     gpio_set_level(BSP_PIN_BLUE_LED, 0);
+    _is_blue_led_on = false;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -18,6 +23,7 @@ void bsp_led_init(void) {
 void bsp_led_set(bool is_on) {
 
     gpio_set_level(BSP_PIN_BLUE_LED, is_on ? 1 : 0);
+    _is_blue_led_on = is_on;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -35,14 +41,14 @@ void bsp_led_off(void) {
 /* -------------------------------------------------------------------------- */
 
 void bsp_led_toggle(void) {
-    int new_level = gpio_get_level(BSP_PIN_BLUE_LED) == 0 ? 1 : 0;
+    int new_level = _is_blue_led_on == false ? 1 : 0;
     gpio_set_level(BSP_PIN_BLUE_LED, new_level);
 }
 
 /* -------------------------------------------------------------------------- */
 
 bool bsp_led_is_on(void) {
-    return gpio_get_level(BSP_PIN_BLUE_LED) == 1;
+    return _is_blue_led_on == true;
 }
 
 /* -------------------------------------------------------------------------- */
